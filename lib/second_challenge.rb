e6d653201abd5b0412dcb3e88e2fac644821ba8b
Sean Conrad <seanconrad123@gmail.com>
2018-06-24 04:52:41 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,8 @@ def second_challenge
   }
 
   #code your solution here!
-  groceries.values :dairy, :vegetable, :meat, :grains
-  print new_array
+  print groceries.values :dairy, :vegetable, :meat, :grains
+  
 end
 
 
