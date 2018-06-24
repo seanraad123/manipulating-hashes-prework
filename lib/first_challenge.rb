53cc3850c39d23cd55edbd490350376aede78193
Sean Conrad <seanconrad123@gmail.com>
2018-06-24 04:23:28 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -12,11 +12,9 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
   #your code here
 
-
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
+  return contacts
   #remember to return your newly altered contacts hash!
-  contacts
 end
-
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,6 +8,6 @@ def second_challenge
 
   #code your solution here!
 
-  
+  puts groceries.values
 
 end
